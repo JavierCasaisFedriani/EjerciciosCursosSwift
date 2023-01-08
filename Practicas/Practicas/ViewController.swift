879891a355172ba7667ecbbedd0c6719f2f97181
JavierCasaisFedriani <javier.casais.fedriani.2021@alumnos.fesac.es>
2023-01-08 18:13:34 +0100

    Crear fichero

---
+++ Practicas/Practicas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practicas
+//
+//  Created by Javier Casais Fredriani on 8/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
